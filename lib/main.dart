20f2cdccd36b72b77dd3598210d5fb76ab1604e6
Gurupra5ad <guruprasad2511@gmail.com>
2023-08-13 00:54:03 +0530

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.orangeAccent[200],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("I am Rich"),
+          backgroundColor: Colors.deepOrange[400],
+        ),
+        body: Center(
+          child: Image(image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
